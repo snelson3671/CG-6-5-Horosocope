0e725a3e2f5c98f6deaa58cec3fcd7c9c75ba43a
Sandra <snelson3671@yahoo.com>
2018-08-19 21:15:27 -0500

    Add project files.

---
+++ CG 6-5 Horosocope/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_6_5_Horosocope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
